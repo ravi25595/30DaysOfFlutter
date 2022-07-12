0e86f7672171126f34bbb9fb63c28d84fb69bc55
ADMIN <ravi25595@gmail.com>
2022-07-12 10:44:36 +0530

    Day 11  Completed Json file reading

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,31 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/models/catalog.dart';
 import 'package:flutter_app/widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     int days = 30;
@@ -16,7 +37,9 @@ class HomePage extends StatelessWidget {
       body: ListView.builder(
         itemCount: dummyList.length,
         itemBuilder: (context, index) {
-          return ItemWidget(item: dummyList[index],);
+          return ItemWidget(
+            item: dummyList[index],
+          );
         },
       ),
       drawer: MyDrawer(),
